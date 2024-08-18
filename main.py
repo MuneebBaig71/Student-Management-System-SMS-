4f7bb62f8c38192accc83786c5bdfa1542aac59e
Muneeb <muneebbaig71@gmail.com>
2024-08-18 18:23:00 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# print('hello world')
+# name = 'muneeb'
+
+# _private_variable = 42
+
+# class MyClass:
+#     def __init__(self):
+#         self._private_instance_variable = "private"
+#         print()
+
+# text = "123"
+# is_decimal = text.isdecimal()
+# print(is_decimal)  # Output: True
+
+# name: int = 123
+# name = 'muneeb'
+# # print(type(name))
+# print(name)
+# name = "muneeb"
+# age = 23
+
+
+name: list[str] = ["muneeb", "ahmed", "ali"]
+print(name.index("muneeb"))
+print(id(list))
+popped_value = name.pop()
+print(id(popped_value))
+print(name, popped_value)
+
+
---
+++ sms.py
@@ -0,0 +1,44 @@
+print("Welcome to the student management system")
+
+name = input("Enter your name: ")
+Roll_no = input("Enter your Roll no: ")
+
+def subjects():
+    print("Enter your subjects")
+    sub1 = "maths"
+    sub2 = "english"
+    sub3 = "computer science"
+    return  sub1, sub2, sub3
+
+
+def marks():
+    sub1, sub2, sub3 = map(int, input("Enter your marks for all three subjects: ").split())
+    return sub1, sub2, sub3
+    
+available_subjects = subjects()
+print("Available subjects are: " ,",".join(available_subjects))
+
+
+
+sub1, sub2, sub3 = marks()
+
+
+if sub1 >= 40:
+        print("Passed")
+        print(f"Maths marks{marks}")
+else:
+        print("Fail in Maths")
+
+if sub2 >= 40:
+        print("Passed")
+        print(f"English marks{marks}")
+else:
+        print("Fail in English")
+
+if sub3 >= 40:
+        print("Passed")
+        print(f"Computer Science marks{marks}")
+else:
+        print("Fail in Computer Science")
+
+
